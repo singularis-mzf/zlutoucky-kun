e030591d1472b19dd625ef13941d8ae9a14ae208
Singularis <singularis@volny.cz>
2020-09-27 12:11:01 +0200

    Zkušební soubory

---
+++ Vytvořit Ignorovaného Koně.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -e
+mkdir 'ignorovaný žluťoučký kůň příšerně úpěl ďábelské ódy, ŽLUŤOUČKÝ KŮŇ PŘÍŠERNĚ ÚPĚL ĎÁBELSKÉ ÓDY' && cp 'žluťoučký kůň příšerně úpěl ďábelské ódy, ŽLUŤOUČKÝ KŮŇ PŘÍŠERNĚ ÚPĚL ĎÁBELSKÉ ÓDY/žluťoučký kůň příšerně úpěl ďábelské ódy, ŽLUŤOUČKÝ KŮŇ PŘÍŠERNĚ ÚPĚL ĎÁBELSKÉ ÓDY.md' 'ignorovaný žluťoučký kůň příšerně úpěl ďábelské ódy, ŽLUŤOUČKÝ KŮŇ PŘÍŠERNĚ ÚPĚL ĎÁBELSKÉ ÓDY'
+
---
+++ žluťoučký kůň příšerně úpěl ďábelské ódy, ŽLUŤOUČKÝ KŮŇ PŘÍŠERNĚ ÚPĚL ĎÁBELSKÉ ÓDY/žluťoučký kůň příšerně úpěl ďábelské ódy, ŽLUŤOUČKÝ KŮŇ PŘÍŠERNĚ ÚPĚL ĎÁBELSKÉ ÓDY.md
@@ -0,0 +1,5 @@
+# žluťoučký kůň příšerně úpěl ďábelské ódy, ŽLUŤOUČKÝ KŮŇ PŘÍŠERNĚ ÚPĚL ĎÁBELSKÉ ÓDY.
+
+## žluťoučký kůň příšerně úpěl ďábelské ódy, ŽLUŤOUČKÝ KŮŇ PŘÍŠERNĚ ÚPĚL ĎÁBELSKÉ ÓDY.
+
+?
